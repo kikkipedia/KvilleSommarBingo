e0cc3fa3a0a9c99e24668b98128fa699de054951
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2024-06-03 20:15:22 +0200

    Added vuetify. Added menu dialogs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    vuetify()
   ],
   resolve: {
     alias: {
